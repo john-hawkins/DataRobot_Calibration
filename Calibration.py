eef236667ab66e50b0dc6c1e6ebac94e335323b5
John Hawkins <john.hawkins@john.hawkins.MacBook>
2019-06-08 21:14:03 +1000

    Added Platt scaling and improved plot

--- Calibration.py
+++ Calibration.py
@@ -2,31 +2,73 @@
 # Methods for re-calibrating the probability outputs of a DataRobot model
 # #######################################################################################
 
+import itertools 
 import datarobot as dr
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 from matplotlib.collections import LineCollection
-from sklearn.linear_model import LinearRegression
 from sklearn.isotonic import IsotonicRegression
 from sklearn.utils import check_random_state
+from sklearn.metrics import (brier_score_loss, precision_score, recall_score)
+from sklearn.linear_model import LogisticRegression
 
-
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+#isotonic = CalibratedClassifierCV(est, cv=2, method='isotonic')
+#sigmoid = CalibratedClassifierCV(est, cv=2, method='sigmoid')
 
 # #######################################################################################
 #  GENERATE A CALIBRATION MODEL
-#  NOTE: The content of actual_col must be numeric
+#  NOTES: The content of actual_col must be numeric
+#         I am wrapping it up in a dictionary with the maximum observed because for some
+#         reason the IR will return NaN for probabilities outside the bounds of the 
+#         training data
 # #######################################################################################
-def generate_calibration_model(df, pred_col, actual_col):
+def generate_calibration_model( df, pred_col, actual_col ):
     ir = IsotonicRegression()
-    y_ = ir.fit_transform(df[pred_col], df[actual_col])
-    return ir
+    y_ = ir.fit_transform( df[pred_col], df[actual_col] )
+    calib = {}
+    calib['method'] = 'ir'
+    calib['mod'] = ir
+    calib['max_obs'] = max( df[pred_col] ) 
+    calib['max_cal'] = max( y_ ) 
+    return calib
 
+def generate_calibration_model_platt( df, pred_col, actual_col ):
+    lr = LogisticRegression()                                                   
+    temp = np.reshape(df[pred_col], (-1, 1)) # LR needs X to be 2-dimensional
+    lr.fit(  temp, df[actual_col] )     
+    y_ = lr.predict_proba( temp )[:,1]
+    calib = {}
+    calib['method'] = 'platt'
+    calib['mod'] = lr
+    calib['max_obs'] = max( df[pred_col] )
+    calib['max_cal'] = max( y_ )
+    return calib
+
+
+#df_calibrate['calibrated'] =  ir.transform( df_calibrate[pred_col] )
+#max_scored = max(df_calibrate['pred_col'])
+#max_calib = max(df_calibrate['calibrated'])
+
+# #######################################################################################
+#  APPLY THE CALIBRATION 
+# #######################################################################################
+def apply_calibration( data, calib ) :
+    temp = data.copy()
+    temp[ temp > calib['max_obs'] ] = calib['max_obs']
+    if calib['method'] == 'platt':
+        temp = np.reshape( data, (-1, 1)) # LR needs X to be 2-dimensional
+        calibrated = calib['mod'].predict_proba( temp )[:,1]
+    else:
+        calibrated =  calib['mod'].transform( temp )
+    return calibrated
 
 # #######################################################################################
 #  GENERATE A RELIABILITY PLOT TO COMPARE THE CALIBRATION OF SCORES
 # #######################################################################################
-def generate_reliability_plot(df, pred_cols, actual_col):
+def generate_reliability_plot( df, pred_cols, actual_col ):
     fig = plt.figure()
     cols = ['r.', 'g.', 'b.', 'c.', 'm.', 'k.', 'y.']
     index = 0
@@ -43,6 +85,29 @@ def generate_reliability_plot(df, pred_cols, actual_col):
     plt.ylabel('Empirical Probability')
     return plt
 
+
+# #######################################################################################
+#  GENERATE A RELIABILITY PLOT WITH DENSITY
+# #######################################################################################
+def plot_calibration_curves( df, pred_cols, actual_col, pred_names, fig_index ):
+    fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for (nam, col) in zip(pred_names, pred_cols):
+        frac_pos, mean_pred_value = calibration_curve(df[actual_col], df[col], n_bins=10)
+        clf_score = brier_score_loss(df[actual_col], df[col], pos_label=1)
+        ax1.plot(mean_pred_value, frac_pos, "s-", label="%s (%1.3f)" % (nam, clf_score))
+        ax2.hist(df[col], range=(0, 1), bins=10, label=nam, histtype="step", lw=2)
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots (reliability curve)')
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+    plt.tight_layout()
+    return plt
 
 
 # #######################################################################################
--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import Calibration as clb
 import datarobot as dr
 import pandas as pd
 
-# LOAD THE SAMPLE DATA THAT WILL BE USED
+# LOAD THE EXAMPLE DATA THAT WILL BE USED
 df_calibrate = pd.read_csv('data/validation_data_with_predictions.csv')
 df_forecast = pd.read_csv('data/holdout_data_with_predictions.csv')
 df_final_forecast = pd.read_csv('data/final_holdout_with_preds.csv')
@@ -12,23 +12,15 @@ df_final_forecast = pd.read_csv('data/final_holdout_with_preds.csv')
 pred_col = "prediction"
 actual_col = 'target'
 
-# CREATE THE CALIBRATION MODEL
-ir = clb.generate_calibration_model(df_calibrate, pred_col, actual_col)
- 
-# APPLY THE CALIBRATION TO THE HOLDOUT
-df_forecast['calibrated'] =  ir.transform( df_forecast[pred_col] )
+# CREATE THE CALIBRATION MODEL USING THE OUT-OF-FOLD PREDICTIONS
+# ON THE INITIAL FIVE FOLD CROSS VALIDATION USING PLATT SCALING
+calib = clb.generate_calibration_model_platt(df_calibrate, pred_col, actual_col)
 
-
-df_final_forecast['calibrated'] =  ir.transform( df_final_forecast[pred_col] )
-
-df_final_forecast['Calibrated Model'] = df_forecast['calibrated']
-
-# PLOT THE ORIGINAL AND CALIBRATED PROBABILITIES
-plt = clb.generate_reliability_plot(df_forecast, [pred_col, 'calibrated'], actual_col)
+# APPLY THE CALIBRATION TO THE HOLDOUT AND PLOT IT
+# COMPARE WITH ORGINAL MODEL AND A MODEL RETRAINED TO USE THE VALIDATION DATA AS WELL
+df_forecast['calibrated'] = clb.apply_calibration( df_forecast[pred_col], calib ) 
+df_forecast['retrained'] = df_final_forecast[pred_col]
+plt = clb.plot_calibration_curves(df_forecast, ['prediction', 'calibrated', 'retrained'], actual_col, ['Original Model', 'Calibrated Model', 'Retrained Model'], 1)
 plt.savefig('results/calibration.png')
 
-plt2 = clb.generate_reliability_plot(df_final_forecast, [ pred_col, 'Calibrated Model'], actual_col)
-plt2.savefig('results/final_calibration.png')
  
-#df_final_forecast.to_csv('results/final_holdout_calibrated.csv')
-
